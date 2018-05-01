577ca72260f0be1fe4151ae00b6283af30ee3f86
Ivan Lucas <igoncalves@thecode.co>
2018-05-01 18:45:13 +1000

    Fixed viewport meta tag content

--- content/themes/wp-react/index.php
+++ content/themes/wp-react/index.php
@@ -3,16 +3,16 @@
  * The main template file
  *
  * @package WordPress
- * @subpackage Celestial
- * @since Celestial 1.0
+ * @subpackage WP React
+ * @since WP React 1.0
  */
- ?>
+?>
  <!DOCTYPE html>
 
  <html <?php language_attributes(); ?> class="no-js">
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, , shrink-to-fit=no, user-scalable=no">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <title>WP React</title>
         <?php wp_head(); ?>
@@ -21,6 +21,6 @@
         <div id="page" class="hfeed site">
             <div id="root"></div>
             <?php wp_footer(); ?>
-        </div>			
+        </div>
     </body>
 </html>
